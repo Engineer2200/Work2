81915bc5620938b6baed696562b2c74d0a060f33
Engineer <arsenya.suvorov@mail.ru>
2022-11-19 18:20:45 +0400

    start

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿ int number = new Random().Next(1 , 155);     
+ Console.WriteLine($"Наше случайное число = {number}");
+
+ if (number>100)
+     {
+        Console.WriteLine(number.ToString()[2]);
+     }
+ else
+ {
+     Console.WriteLine("Нет третьей цифры ");
+ }
+
