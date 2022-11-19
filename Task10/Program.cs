d82fe0ba18ccdb66e1432abceab4ec9e57875c93
Engineer <arsenya.suvorov@mail.ru>
2022-11-19 16:34:39 +0400

    star

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100 , 1000);
+Console.WriteLine($"Наше случайное число = {number}");
+int number1 = number % 100;
+int number2 = number1 / 10;
+
+Console.WriteLine(number2);
